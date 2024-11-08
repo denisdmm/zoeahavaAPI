3901d7c97b07d885ad2bdc0f971de8d5b6bf030b
Denis <denisdmm@gmail.com>
2024-11-08 16:07:06 -0300

    ajustando enblecors para outros domiios.

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,19 @@ async function bootstrap() {
   }))
 
   app.enableCors({
-    origin: 'https://alfazoeahava.netlify.app',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'https://alfazoeahava.netlify.app',
+        'http://localhost:4200', // Adicione os domínios que você deseja permitir
+        'https://coroeorquestra.netlify.app',
+      ];
+      
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Origin not allowed by CORS'));
+      }
+    },
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
